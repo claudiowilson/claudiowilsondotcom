6bf380b7f33aafc701232199af3ed0b8fb73d4af
Claudio Wilson <claudiowilson@utexas.edu>
2014-05-05 00:48:15 -0500

    initial commit of firebase backend so that users can add albums

--- web.js
+++ web.js
@@ -1,20 +1,31 @@
 var express = require('express'),
 	stylus = require('stylus'),
-	async = require('async');
-var app = express();
+	async = require('async'),
+	Firebase = require('firebase');
 
-app.configure(function() {
-	app.use(express.logger('dev'));
-	app.use(stylus.middleware({
-		src: __dirname + '/views',
-		dest: __dirname + '/public'
-	}));
-	app.set('views', __dirname + '/views');
-	app.use(express.static(__dirname + '/public'));
+var app = express();
+app.use(stylus.middleware({
+	src: __dirname + '/views',
+	dest: __dirname + '/public'
+}));
+
+var albumRef = new Firebase('https://glowing-fire-8113.firebaseio.com/albums');
+var albumCache = [];
+
+albumRef.once('value', function(albums) {
+	var obj = albums.val();
+	for (var property in obj) {
+		if(obj.hasOwnProperty(property)) {
+			albumCache.push(obj[property]);
+		}
+	}
 });
 
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
-	response.render('index.jade');
+	response.render('index.jade', {albums: JSON.stringify(albumCache)});
 });
 
 var port = process.env.PORT || 3000;
