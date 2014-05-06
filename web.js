f163d5e18607841c6f74437d2af3e1c179e64a89
Claudio Wilson <claudiowilson@utexas.edu>
2014-05-05 23:18:26 -0500

    added auth for firebase, fix up math for svg background

--- web.js
+++ web.js
@@ -1,30 +1,40 @@
 var express = require('express'),
 	stylus = require('stylus'),
-	async = require('async'),
-	Firebase = require('firebase');
+	Firebase = require('firebase'),
+	FirebaseTokenGenerator = require('firebase-token-generator');
 
 var app = express();
 app.use(stylus.middleware({
 	src: __dirname + '/views',
 	dest: __dirname + '/public'
 }));
+var tokenGenerator = new FirebaseTokenGenerator('GyzhDVAARVJ01NtpPDcqUbBJzK0WUyVDXuGauqQr');
+var token = tokenGenerator.createToken();
 
 var albumRef = new Firebase('https://glowing-fire-8113.firebaseio.com/albums');
 var albumCache = [];
 
-albumRef.once('value', function(albums) {
-	var obj = albums.val();
-	for (var property in obj) {
-		if(obj.hasOwnProperty(property)) {
-			albumCache.push(obj[property]);
-		}
+albumRef.auth(token, function(err) {
+	if(err) {
+		console.log("Authentication failed!");
+	} else {
+		console.log("Login succeeded!");
 	}
+});
+
+albumRef.on('child_added', function(snapshot) {
+	console.log(JSON.stringify(snapshot.val()) + " added!");
+	albumCache.push(snapshot.val());
 });
 
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
+	response.render('layout.jade', {albums: JSON.stringify(albumCache)});
+});
+
+app.get('/index', function(request, response) {
 	response.render('index.jade', {albums: JSON.stringify(albumCache)});
 });
 
