273cc4a3c8d3c14a4603bc4de58001ba21d467ed
Claudio Wilson <claudiowilson@utexas.edu>
2014-06-18 00:15:07 -0500

    update packagejson to include marked and add post parser

--- postParser.js
+++ postParser.js
@@ -1,18 +1,30 @@
 var fs = require('fs'),
+	marked = require('marked'),
 	folderName = './posts',
 	regex = '([0-9]*-[0-9]*-[0-9]*)_([a-zA-Z ]*)\.md';
 
-var getPosts = function() {
+var getPosts = function(callback) {
 	fs.readdir(folderName, function(err, files) {
-		return files.filter(function(name) {
-			if(name.match(regex)) {
-				return true;
-			}
-			return false;
+		var correct = files.map(function(name) {
+			return getDateAndTitle(name);
 		});
+		callback(null, correct);
+	});
+}
+
+var getDateAndTitle = function(postFileName) {
+	var groups = postFileName.match(regex);
+	if (groups) {
+		return { date : groups[1], title : groups[2], file: postFileName };
+	}
+}
+
+var getHtmlFromMd = function(post, callback) {
+	fs.readFile(folderName + '/' + post, 'ascii', function(err, data) {
+		return marked(data, callback);
 	});
 }
 
 
-
+exports.getHtmlFromMd = getHtmlFromMd;
 exports.getPosts = getPosts;
